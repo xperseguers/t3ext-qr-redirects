2d94771ce77e1e833db6188eb738503cc00f8a1f
Ruben Silva <ruben@hemmer.ch>
2023-07-14 17:05:07 +0200

    [TASK] Create a ModuleController class

---
+++ Classes/Controller/ModuleController.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace Causal\QrRedirects\Controller;
+
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+class ModuleController extends ActionController
+{
+
+    public function createAction(): void
+    {
+
+    }
+
+    public function deleteAction(): void
+    {
+
+    }
+
+    public function overviewAction(): void
+    {
+
+    }
+
+    public function updateAction(): void
+    {
+
+    }
+}
